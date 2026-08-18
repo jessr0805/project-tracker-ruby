542b44e634dad9b1bb87e93e3444d8338cb1063f
jessr0805 <jessesodhi13@gmail.com>
2026-08-18 12:07:23 -0400

    feat: add project categories and completion handling

--- main.rb
+++ main.rb
@@ -24,24 +24,41 @@ loop do
 
   case option
   when 1
-    puts "Here are your active projects:\n#{projects["active"]}"
-    puts "Here are your completed projects: \n#{projects["completed"]}"
+    puts "Here are your active projects: "
+    projects["active"].each do |project|
+        puts "#{project["name"]} - #{project["category"]}"
+    end
+    puts "Here are your completed projects: "
+    projects["completed"].each do |project|
+        puts "#{project["name"]} - #{project["category"]}"
+    end
 
   when 2
     puts "What is the name of the project you would like to add?"
-    new_proj = gets.chomp.downcase
+    new_proj_name = gets.chomp.downcase
+    puts "What is the category of #{new_proj_name}?"
+    new_proj_category = gets.chomp.downcase
+    new_proj = {
+        "name" => new_proj_name,
+        "category" => new_proj_category
+    } 
     projects["active"] << new_proj
     puts "Great! Your project has been added"
   when 3
-    puts "Active projects: #{projects["active"]}"
     puts "Which project have you completed?"
     complete_input = gets.chomp.downcase
-    if projects["active"].include?(complete_input)
-        projects["active"].delete(complete_input)
-        projects["completed"] << complete_input
-        puts "#{complete_input} has been moved to your completed projects! Congrats on finishing!"
+    found = false
+    projects["active"].each do |project|
+        if project["name"] == complete_input
+            projects["active"].delete(project)
+            projects["completed"] << project
+            found = true
+        end
+    end
+    if found == false
+        puts "We weren't able to find your project titled '#{complete_input}'"
     else
-        puts "#{complete_input} is not one of your projects. Please double check your spelling. Your active projects are as follows: #{projects["active"]}"
+        puts "#{complete_input} has been moved to your completed projects! Congrats on finishing!"
     end
   when 4
     puts "Are you sure you want to leave?"
